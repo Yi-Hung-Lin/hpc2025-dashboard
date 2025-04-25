66eb315cbc4f255ffee1e4f0e1bc9ad929e96aac
112062303 <caillou0608@gmail.com>
2025-04-25 23:17:53 +0800

    網站的基本框架與資料夾的建立，html、css、typescript

---
+++ public/ts/config.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyBvBMSzqJuOOsSIEhyM-sVI-vinaIu2GC8",
+    authDomain: "midterm-project-ab3aa.firebaseapp.com",
+    databaseURL: "https://midterm-project-ab3aa-default-rtdb.firebaseio.com",
+    projectId: "midterm-project-ab3aa",
+    storageBucket: "midterm-project-ab3aa.firebasestorage.app",
+    messagingSenderId: "13475203739",
+    appId: "1:13475203739:web:f722da780a822b88ff543e",
+    measurementId: "G-LB3317B8W5",
+    databaseURL: "https://midterm-project-ab3aa-default-rtdb.firebaseio.com"
+};
+firebase.initializeApp(config);
